ec0babf089caf273e1f128f60d87b8bf7a528e2a
Belen Schmid <belenyschmid@gmail.com>
2019-10-01 22:04:42 -0300

    modificacion de contenido

--- faq.php
+++ faq.php
@@ -15,65 +15,60 @@
                 <div class="row bar">
                     <div class="col-md-9">
                         <section>
-                            <p class="lead">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                            <p class="lead">En la sección de Preguntas Frecuentes encontrará información útil en un solo click!</p>
                             <div id="accordion" role="tablist">
                                 <div class="card card-primary">
                                     <div id="headingOne" role="tab" class="card-header">
-                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">1. What to do if I have still not received the order?</a></h5>
+                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">1. ¿Cuánto es el tiempo estipulado de demora en el envío del producto?</a></h5>
                                     </div>
                                     <div id="collapseOne" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" class="collapse show">
                                         <div class="card-body">
-                                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                                            <p>Al contar con un servicio propio de envío en general nuestros productos llegan a cualquier punto del país en menos tiempo.</p>
                                             <ul>
-                                                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                                                <li>Aliquam tincidunt mauris eu risus.</li>
-                                                <li>Vestibulum auctor dapibus neque.</li>
+                                                <li>Córdoba, Mendoza, San Luis, Buenos Aires y Santa Fe: 3 días hábiles</li>
+                                                <li>Norte de Argentina: 4 días hábiles</li>
+                                                <li>Sur de Argentina: 5 días hábiles</li>
                                             </ul>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card card-primary">
                                     <div id="headingTwo" role="tab" class="card-header">
-                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="collapsed">2. What are the postal rates?</a></h5>
+                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" class="collapsed">2. ¿Cómo es la política de cambios?</a></h5>
                                     </div>
                                     <div id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion" class="collapse">
-                                        <div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div>
+                                        <div class="card-body">A partir de la recepción del producto, el cliente tiene 20 días hábiles para generar el cambio, cuyo costo lo afronta VISTE.ME.</div>
                                     </div>
                                 </div>
                                 <div class="card card-primary">
                                     <div id="headingThree" role="tab" class="card-header">
-                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="collapsed"> 3. Do you send overseas?</a></h5>
+                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="collapsed"> 3. ¿Realizan envíos al exterior?</a></h5>
                                     </div>
                                     <div id="collapseThree" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion" class="collapse">
-                                        <div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div>
+                                        <div class="card-body">Desde el año 2016 realizamos envíos a todo Latinoamérica y el Caribe. Estimamos este año hacer envíos a Europa y Asia.</div>
                                     </div>
                                 </div>
                                 <div class="card card-primary">
                                     <div id="headingFour" role="tab" class="card-header">
-                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive" class="collapsed">  4. Why are you more expensive than others?</a></h5>
+                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive" class="collapsed">  4. ¿Cuáles son los métodos de pago aceptados?</a></h5>
                                     </div>
                                     <div id="collapseFive" role="tabpanel" aria-labelledby="headingFour" data-parent="#accordion" class="collapse">
                                         <div class="card-body">
-                                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                                            <ul>
-                                                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                                                <li>Aliquam tincidunt mauris eu risus.</li>
-                                                <li>Vestibulum auctor dapibus neque.</li>
-                                            </ul>
+                                            <p>Es posible abonar mediante tarjetas de crédito (VISA, MASTERCARD y AMERICAN EXPRESS) y tarjetas de débito (VISA y MAESTRO).</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="card card-primary">
                                     <div id="headingFive" role="tab" class="card-header">
-                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="collapsed"> 5. Another important question</a></h5>
+                                        <h5 class="mb-0 mt-0"><a data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" class="collapsed"> 5. ¿Dónde se pueden efectuar reclamos?</a></h5>
                                     </div>
                                     <div id="collapseThree" role="tabpanel" aria-labelledby="headingFive" data-parent="#accordion" class="collapse">
                                         <div class="card-body">
-                                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                                            <p>En caso que tengas algún inconveniente con tu compra, sugerencias o reclamos, no dudes en contactarte a: </p>
                                             <ul>
-                                                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                                                <li>Aliquam tincidunt mauris eu risus.</li>
-                                                <li>Vestibulum auctor dapibus neque.</li>
+                                                <li>atencionalcliente@visteme.com</li>
+                                                <li>reclamos@visteme.com</li>
+                                                <li>consultas@visteme.com</li>
                                             </ul>
                                         </div>
                                     </div>
@@ -84,20 +79,7 @@
                     </div>
                     <div class="col-sm-3">
                         <!-- PAGES MENU -->
-                        <div class="panel panel-default sidebar-menu">
-                            <div class="panel-heading">
-                                <h3 class="h4 panel-title">Pages</h3>
-                            </div>
-                            <div class="panel-body">
-                                <ul class="nav nav-pills flex-column text-sm">
-                                    <li class="nav-item"><a href="-back-template/text.html" class="nav-link">Text page</a></li>
-                                    <li class="nav-item"><a href="-back-template/contact.html" class="nav-link">Contact page</a></li>
-                                    <li class="nav-item"><a href="-back-template/faq.html" class="nav-link">FAQ</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="banner"><a href="shop-category.html"><img src="img/banner.jpg" alt="sales 2014" class="img-fluid"></a></div>
-                    </div>
+                        
                 </div>
             </div>
         </div>
