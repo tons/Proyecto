41a350d15a5cfc0564f46a15e09a3deb3886d9f9
Gaston Fontaine <gastonfontaine@gmail.com>
2019-10-01 22:06:37 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ inc/func.php
@@ -0,0 +1,18 @@
+<?php
+
+function chequearEmailDuplicado($email){
+    if (file_exists("db/usuarios.json")) {
+        // Se obtiene texto plano
+        $stringDesdeArchivo = file_get_contents("db/usuarios.json");
+        // El texto plano se convierte a Php
+        $usuarios = json_decode($stringDesdeArchivo, true);
+
+        foreach ($usuarios as $usuario) {
+            if ($usuario["email"] == $email) {
+                return true;
+            }
+            return false;
+        }
+    }
+    return false;
+}
--- registro.php
+++ registro.php
@@ -1,61 +1,75 @@
 <?php
+
 $errores = [];
-if($_POST){
-  $usuario = [
-    "name"=> $_POST["Nombre"],
-    "email"=>$_POST["Email"],
-    "password"=> password_hash($_POST["Password"],PASSWORD_DEFAULT)
-  ];
+$name = $mail = '';
 
- // Se obtiene texto plano
-  $stringDesdeArchivo = file_get_contents("usuarios.json");
-  // El texto plano se convierte a Php
-  $usuarios = json_decode($stringDesdeArchivo, true);
+if (isset($_POST["registry"]) && $_POST["registry"] == "registrarse") {
 
-    // Instrucciones, validaciones,
+    $nombre = $_POST["name"];
+    $mail = $_POST["mail"];
+    $pass = $_POST["password"];
+    $image = $_FILES["image"];
 
-    // Se vuelve a guardar como texto plano
-  file_put_contents("usuarios.json", json_encode($usuarios));
+    if (!isset($_POST['name']) || !isset($_POST['mail']) || !isset($_POST['password'])) {
+        $errores[] = "Debes completar ambos campos";
+    } else {
 
-  $usuarioDuplicado=chequearEmailDuplicado($_POST["email"]);
+        if (empty($_POST['name'])) {
+            $errores[] = "Debes completar el nombre";
+        } else if (strlen($_POST['name']) < 5) {
+            $errores[] = "El nombre debe tener al menos 5 caracteres";
+        }
 
-  if($usuarioDuplicado){
-    $errores[]="El email ya existe";
-    return;
-  }
-
-}
-
-function chequearEmailDuplicado($email){
-  foreach ($usuarios as $usuario) {
-    if($usuario["email"] == $email){
-return true;
-    }
-    return false;
-  }
-}
- // Validación
-if ($_POST) {
-  if (strlen ($_POST['nombre']) == 0){
-    echo "No llenaste el nombre <br>";
-  } if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false) {
-    echo "El mail no tiene el formato correcto";
-  }
-}
-
-// persistencia php//
-$email = "";
-$password = "";
-$nombre = "";
-
- if ($_POST) {
-   $email = $_POST["email"];
-   $password = $_POST["password"];
-   $nombre = $_POST["nombre"];
+        if (empty($_POST['mail'])) {
+            $errores[] = "Debes completar el email";
+        } else if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+            $errores[] = "Debes ingresar un email válido";
+        } else {
+            $existe = chequearEmailDuplicado($_POST["email"]);
+            if($existe){
+                $errores[] = "El email ya existe";
+                return;
+            }
+        }
     }
 
+    if (empty($errores)) {
 
- ?>
+        $usuario = [
+            "name" => $_POST['name'],
+            "password" => password_hash($_POST['password'], PASSWORD_DEFAULT),
+            "email" => $_POST['mail'],
+            "imagen" => $_FILES["image"]
+        ];
+
+        $archivoImagen = $_FILES['image'];
+
+        if (isset($_FILES['image'])) {
+            $extensionImagen = pathinfo($archivoImagen['name'], PATHINFO_EXTENSION);
+            $rutaImagen = 'imagenes/usuarios/' . $_POST['username'] . '.' . $extensionImagen;
+            move_uploaded_file(
+                $archivoImagen['tmp_name'],
+                $rutaImagen
+            );
+
+            $usuario['image'] = $rutaImagen;
+        }
+
+        $datosPuros = file_get_contents('usuarios.json');
+        $datosEnPhp = json_decode($datosPuros, true);
+
+        $usuario['id'] = (count($datosEnPhp['usuarios']) + 1);
+
+        $datosEnPhp['usuarios'][] = $usuario;
+
+        file_put_contents('usuarios.json', json_encode($datosEnPhp));
+
+
+        return header("Location: index.php");
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -80,32 +94,36 @@ $nombre = "";
                             <p class="text-muted">Si tenes alguna pregunta, no dudes en <a href="-back-template/contact.html">contactarnos</a>,
                                 nuestro centro de atención al cliente está a disposición las 24 hs.</p>
                             <hr>
+                            <?php if(count($errores)) {
+                                echo "<h3>Revise Errores:</h3><ul>";
+                                foreach ($errores as $item) {
+                                    echo "<li>".$item."</li>";
+                                }
+                                echo "<ul>";
+                            } ?>
                             <form action="customer-orders.html" method="post">
                                 <div class="form-group">
                                     <label for="name-login">Nombre</label>
-                                    <input id="name" type="text" class="form-control" name="Nombre">
+                                    <input id="name" type="text" class="form-control" name="Nombre" value="">
                                 </div>
                                 <div class="form-group">
                                     <label for="email-login">Email</label>
-                                    <input id="email" type="text" class="form-control" name="Email">
+                                    <input id="email" type="text" class="form-control" name="Email" value="">
                                 </div>
                                 <div class="form-group">
                                     <label for="password-login">Contraseña</label>
-                                    <input id="password" type="password" class="form-control" name="Password">
+                                    <input id="password" type="password" class="form-control" name="Password" value="">
                                 </div>
                                 <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="customFileLang" lang="es">
                                     <label class="custom-file-label" for="customFileLang">Sube tu foto de perfil</label>
+                                    <input type="file" name="image" class="custom-file-input" id="image" lang="es">
                                 </div>
-                                <div class="text-center">
                                 <div class="form-group">
-                                <div class="form-check">
-                                  <input class="form-check-input" type="checkbox" id="recordar usuario">
-                                  <label class="form-check-label" for="gridCheck">
-                                    Recordar Usuario
-                                  </label>
+                                    <div class="form-check">
+                                      <input class="form-check-input" type="checkbox" id="recordar usuario">
+                                      <label class="form-check-label" for="gridCheck">Recordar Usuario</label>
+                                    </div>
                                 </div>
-                              </div>
                                     <button type="submit" class="btn btn-template-outlined"><i class="fa fa-user-md"></i> Registrarse</button>
                                 </div>
                             </form>
